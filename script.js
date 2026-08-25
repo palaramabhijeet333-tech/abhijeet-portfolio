cef8159d872e08f23907717ed3325b96582583b3
palaramabhijeet333-tech <palaramabhijeet333@gmail.com>
2026-08-25 23:48:02 +0530

    added dark mode local storage logic

--- script.js
+++ script.js
@@ -2,15 +2,34 @@ const darkMode = document.getElementById("darkMode");
 const darkModeContainer = document.getElementById("dark-light");
 const blueBg = document.getElementById("blueBg")
 
+function updateTheme(isDarkMode){
+    if(isDarkMode){
+        document.body.classList.add("dark-mode");
+          darkModeContainer.innerHTML = '<i class="fa-regular fa-sun"></i>';
+        blueBg.src = "assests/images/white background.png";
+    }else{
+        document.body.classList.remove("dark-mode")
+          darkModeContainer.innerHTML = '<i class="fa-regular fa-moon"></i>';
+        blueBg.src = "assests/images/blue background.png";
+    }
+}
+
+let savedTheme = localStorage.getItem("theme");
+if(savedTheme ==="dark"){
+    updateTheme(true);
+}else{
+    updateTheme(false);
+}
 
 darkModeContainer.addEventListener("click",()=>{
 
-    document.body.classList.toggle("dark-mode");
-    if( document.body.classList.contains("dark-mode")){
-        darkModeContainer.innerHTML = '<i class="fa-regular fa-sun"></i>'
-        blueBg.src = "assests/images/white background.png"
+    const isDarkMode = document.body.classList.contains("dark-mode");
+    console.log(isDarkMode)
+    if(isDarkMode){
+        localStorage.setItem("theme","light");
+        updateTheme(false)
     }else{
-        darkModeContainer.innerHTML = '<i class="fa-regular fa-moon"></i>'
-        blueBg.src = "assests/images/blue background.png"
+        localStorage.setItem("theme","dark");
+        updateTheme(true)
     }
 })
